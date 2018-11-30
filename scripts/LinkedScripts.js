eb06059085f79ece6ae61c5395b92bf1564db0ff
ColdPOM <coldpom.games@gmail.com>
2018-11-30 15:00:22 +0900

    フォルダ整理とES6対応

---
+++ scripts/LinkedScripts.js
@@ -0,0 +1,9 @@
+// ---------------------------------------------------------------------------------
+/// 使用するプログラムファイルのリンク
+// ---------------------------------------------------------------------------------
+
+document.write(
+	'<script type="text/javascript" src="lib/Pomlib/PomLib.js"></script>',
+	'<script type="text/javascript" src="scripts/js/Main.js"></script>',
+	'<script type="text/javascript" src="scripts/js/GameMain.js"></script>'
+);
